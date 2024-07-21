1f64f4e5f1e3e2dd15353ed4ceda8adc8c119d1f
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-21 22:02:29 +0100

    detect section with player in

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -22,7 +22,10 @@ impl Plugin for GamePlugin {
         app.add_sub_state::<GameState>()
             .add_systems(Startup, setup_game)
             .add_systems(OnEnter(GameState::Running), spawn_base_game)
-            .add_systems(Update, player_run.run_if(in_state(GameState::Running)));
+            .add_systems(
+                Update,
+                (player_run, section_detect_player).run_if(in_state(GameState::Running)),
+            );
     }
 }
 
@@ -45,6 +48,12 @@ pub struct GameImage {
 #[derive(Resource, Debug, Clone, PartialEq, Eq)]
 pub struct GameRenderLayer {
     layer: RenderLayers,
+}
+
+#[derive(Resource, Debug, Clone, PartialEq, Eq)]
+pub struct SectorMaterials {
+    default: Handle<ColorMaterial>,
+    with_player: Handle<ColorMaterial>,
 }
 
 #[derive(Component, Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -126,17 +135,21 @@ fn spawn_base_game(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
+    let section_default_material = materials.add(Color::srgb(0.7, 0.7, 0.7));
+    let section_with_player_material = materials.add(Color::srgb(0.2, 0.8, 0.2));
+
     let mesh = meshes.add(CircularSector::new(200.0, std::f32::consts::FRAC_PI_8));
 
     // Sectors
-    for i in 1..9 {
-        let material = materials.add(Color::srgb(0.8 / i as f32, 0.8 / i as f32, 0.8 / i as f32));
+    for i in 0..8 {
         let mut transform = Transform::from_xyz(0.0, 0.0, 0.0);
-        transform.rotate_local_z(std::f32::consts::FRAC_PI_4 * (i - 1) as f32);
+        // Rotate PI/8 more to start section at 0/12
+        transform
+            .rotate_local_z(std::f32::consts::FRAC_PI_8 + std::f32::consts::FRAC_PI_4 * i as f32);
         commands.spawn((
             MaterialMesh2dBundle {
                 mesh: mesh.clone().into(),
-                material,
+                material: section_default_material.clone(),
                 transform,
                 ..default()
             },
@@ -148,11 +161,10 @@ fn spawn_base_game(
 
     // Center
     let mesh = meshes.add(Circle { radius: 180.0 });
-    let material = materials.add(Color::srgb(0.8, 0.8, 0.8));
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: mesh.into(),
-            material,
+            material: section_default_material.clone(),
             transform: Transform::from_xyz(0.0, 0.0, 1.0),
             ..default()
         },
@@ -160,6 +172,11 @@ fn spawn_base_game(
         game_render_layer.layer.clone(),
         StateScoped(GlobalState::InGame),
     ));
+
+    commands.insert_resource(SectorMaterials {
+        default: section_default_material,
+        with_player: section_with_player_material,
+    });
 
     let mesh = meshes.add(Circle { radius: 20.0 });
     let material = materials.add(Color::srgb(0.1, 0.9, 0.2));
@@ -173,7 +190,7 @@ fn spawn_base_game(
             ..default()
         },
         Player,
-        PlayerSpeed(0.1),
+        PlayerSpeed(0.5),
         Wireframe2d,
         game_render_layer.layer.clone(),
         StateScoped(GlobalState::InGame),
@@ -191,4 +208,34 @@ fn player_run(time: Res<Time>, mut player: Query<(&PlayerSpeed, &mut Transform)>
 
     transform.translation = rotated;
     transform.rotation *= rotation;
+}
+
+fn section_detect_player(
+    section_materials: Res<SectorMaterials>,
+    player: Query<&Transform, With<Player>>,
+    mut sections: Query<(&CircleSector, &mut Handle<ColorMaterial>)>,
+) {
+    let Ok(player_transform) = player.get_single() else {
+        return;
+    };
+
+    let to_center = player_transform.translation.normalize();
+    let angle = to_center.angle_between(Vec3::Y);
+
+    let mut section_id = (angle / std::f32::consts::FRAC_PI_4).floor() as u8;
+
+    if 0.0 < player_transform.translation.x {
+        section_id = 7 - section_id;
+    }
+
+    let previous_section_id = if section_id == 7 { 0 } else { section_id + 1 };
+
+    for (section, mut material_handle) in sections.iter_mut() {
+        if section.0 == section_id {
+            *material_handle = section_materials.with_player.clone();
+        }
+        if section.0 == previous_section_id {
+            *material_handle = section_materials.default.clone();
+        }
+    }
 }
