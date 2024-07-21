bce9e835fa63f8e7206fd8ce967015cb8016b8ee
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-21 17:40:57 +0100

    add nix shell and cargo config files

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  shellHook = ''export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+    pkgs.vulkan-loader
+    pkgs.libxkbcommon
+    pkgs.wayland
+    pkgs.udev
+    pkgs.alsaLib
+  ]}"'';
+  buildInputs = with pkgs; [
+    wayland
+    mold
+    pkg-config
+    udev
+    alsaLib
+  ];
+}
