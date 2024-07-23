1478deca7ce643d68a17672fe6c604f52446ee4b
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-23 18:18:05 +0100

    basic lightning and heal spells

--- src/game/spells.rs
+++ src/game/spells.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use super::GameState;
+use super::{enemy::BattleEnemy, Defense, GameState, Health, Player};
 
 pub struct SpellsPlugin;
 
@@ -8,7 +8,10 @@ impl Plugin for SpellsPlugin {
     fn build(&self, app: &mut App) {
         app.add_event::<CastSpell>()
             .add_systems(Startup, prepare_spells)
-            .add_systems(Update, cast_spell.run_if(in_state(GameState::Battle)));
+            .add_systems(
+                Update,
+                (cast_spell, process_lightninig, process_heal).run_if(in_state(GameState::Battle)),
+            );
     }
 }
 
@@ -18,26 +21,34 @@ pub struct SpellIdx(pub usize);
 #[derive(Event, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct CastSpell(pub SpellIdx);
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum Spell {
-    Lightning,
-    Heal,
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Lightning {
+    strikes: u32,
+    delta_time: f32,
+    damage: f32,
 }
 
-impl Spell {
-    pub fn duration(&self) -> Option<f32> {
-        match self {
-            Self::Lightning => None,
-            Self::Heal => None,
-        }
-    }
+#[derive(Component, Debug, Clone)]
+pub struct LightningSpell {
+    timer: Timer,
+    remaining_strikes: u32,
+    damage: f32,
+}
 
-    pub fn cooldown(&self) -> f32 {
-        match self {
-            Self::Lightning => 2.0,
-            Self::Heal => 5.0,
-        }
-    }
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Heal {
+    heal: f32,
+}
+
+#[derive(Component, Debug, Clone)]
+pub struct HealSpell {
+    heal: f32,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Spell {
+    Lightning(Lightning),
+    Heal(Heal),
 }
 
 #[derive(Debug)]
@@ -56,27 +67,85 @@ fn prepare_spells(mut commands: Commands) {
     spells.0.push(SpellInfo {
         name: "Lightning",
         drop_rate: 0.9,
-        spell: Spell::Lightning,
+        spell: Spell::Lightning(Lightning {
+            strikes: 2,
+            delta_time: 0.1,
+            damage: 1.0,
+        }),
     });
     spells.0.push(SpellInfo {
         name: "Heal",
         drop_rate: 0.9,
-        spell: Spell::Heal,
+        spell: Spell::Heal(Heal { heal: 20.0 }),
     });
 
     commands.insert_resource(spells);
 }
 
-fn cast_spell(spells: Res<Spells>, mut event_reader: EventReader<CastSpell>) {
+fn cast_spell(
+    spells: Res<Spells>,
+
+    mut commands: Commands,
+    mut event_reader: EventReader<CastSpell>,
+) {
     for e in event_reader.read() {
         let spell_info = &spells.0[e.0 .0];
         match spell_info.spell {
-            Spell::Lightning => {
-                println!("casting Lightning");
+            Spell::Lightning(lightning) => {
+                commands.spawn(LightningSpell {
+                    timer: Timer::from_seconds(lightning.delta_time, TimerMode::Repeating),
+                    remaining_strikes: lightning.strikes,
+                    damage: lightning.damage,
+                });
             }
-            Spell::Heal => {
-                println!("casting Heal");
+            Spell::Heal(heal) => {
+                commands.spawn(HealSpell { heal: heal.heal });
             }
         }
+    }
+}
+
+fn process_lightninig(
+    time: Res<Time>,
+    mut commands: Commands,
+    mut lightnings: Query<(Entity, &mut LightningSpell)>,
+    mut enemy: Query<(&Defense, &mut Health), With<BattleEnemy>>,
+) {
+    let Ok((enemy_defense, mut enemy_health)) = enemy.get_single_mut() else {
+        return;
+    };
+
+    for (lightning_entity, mut lightning) in lightnings.iter_mut() {
+        lightning.timer.tick(time.delta());
+        if lightning.timer.finished() {
+            let damage = lightning.damage * (1.0 - enemy_defense.0);
+            enemy_health.0 -= damage;
+            lightning.remaining_strikes -= 1;
+
+            if lightning.remaining_strikes == 0 {
+                commands
+                    .get_entity(lightning_entity)
+                    .unwrap()
+                    .despawn_recursive()
+            }
+        }
+    }
+}
+
+fn process_heal(
+    heals: Query<(Entity, &HealSpell)>,
+    mut commands: Commands,
+    mut player: Query<&mut Health, With<Player>>,
+) {
+    let Ok(mut player_health) = player.get_single_mut() else {
+        return;
+    };
+
+    for (heal_entity, heal) in heals.iter() {
+        player_health.0 += heal.heal;
+        commands
+            .get_entity(heal_entity)
+            .unwrap()
+            .despawn_recursive()
     }
 }
