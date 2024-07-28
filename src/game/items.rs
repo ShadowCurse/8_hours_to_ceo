260b508a400750df0dcb1cc35653c289a2bf92db
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-07-28 19:03:36 +0100

    update items

--- src/game/items.rs
+++ src/game/items.rs
@@ -15,40 +15,51 @@ pub struct ItemIdx(pub usize);
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub enum Item {
-    Scissors,
-    Bucket,
+    Coffecup,
+    Paperclip,
     Plant,
+    Scissiors,
+    Stapler,
+    Stickynotes,
 }
 
 impl Item {
     pub fn add_damage(&self) -> f32 {
         match self {
-            Self::Scissors => 10.0,
-            Self::Bucket => 0.0,
+            Self::Coffecup => 0.0,
+            Self::Paperclip => 0.0,
             Self::Plant => 0.0,
+            Self::Scissiors => 10.0,
+            Self::Stapler => 5.0,
+            Self::Stickynotes => 0.0,
         }
     }
 
     pub fn add_defense(&self) -> f32 {
         match self {
-            Self::Scissors => 0.0,
-            Self::Bucket => 0.1,
+            Self::Coffecup => 0.0,
+            Self::Paperclip => 0.05,
             Self::Plant => 0.0,
+            Self::Scissiors => 0.0,
+            Self::Stapler => 0.0,
+            Self::Stickynotes => 0.01,
         }
     }
 
     pub fn heal(&self) -> f32 {
         match self {
-            Self::Scissors => 0.0,
-            Self::Bucket => 0.0,
-            Self::Plant => 5.0,
+            Self::Coffecup => 5.0,
+            Self::Paperclip => 0.0,
+            Self::Plant => 1.0,
+            Self::Scissiors => 0.0,
+            Self::Stapler => 0.0,
+            Self::Stickynotes => 0.0,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct ItemInfo {
-    pub name: &'static str,
     pub description: &'static str,
     pub image: Handle<Image>,
     pub drop_rate: f32,
@@ -75,25 +86,40 @@ fn prepare_items(asset_server: Res<AssetServer>, mut commands: Commands) {
     let mut items = Items(vec![]);
 
     items.0.push(ItemInfo {
-        name: "Scissors",
-        description: "Sharp scissors",
-        image: asset_server.load("items/tmp_item.png"),
-        drop_rate: 0.9,
-        item: Item::Scissors,
+        description: "Smoking hot coffe for burnout nerves. Heals 5 hp.",
+        image: asset_server.load("items/item_coffecup.png"),
+        drop_rate: 0.2,
+        item: Item::Coffecup,
     });
     items.0.push(ItemInfo {
-        name: "Bucket",
-        description: "Strudy bucket",
-        image: asset_server.load("items/tmp_item.png"),
+        description: "As paperclip holds papers, you hold your ground. Adds 5% defence.",
+        image: asset_server.load("items/item_paperclip.png"),
         drop_rate: 0.9,
-        item: Item::Bucket,
+        item: Item::Paperclip,
     });
     items.0.push(ItemInfo {
-        name: "Plant",
-        description: "Green plant",
-        image: asset_server.load("items/tmp_item.png"),
+        description: "Decorative plant. Eat a leaf after each battle to restore 1 hp.",
+        image: asset_server.load("items/item_pot.png"),
         drop_rate: 0.9,
         item: Item::Plant,
+    });
+    items.0.push(ItemInfo {
+        description: "Scissors to cut through your opponent's arguments. Add 10 damage.",
+        image: asset_server.load("items/item_scissors.png"),
+        drop_rate: 0.9,
+        item: Item::Scissiors,
+    });
+    items.0.push(ItemInfo {
+        description: "Stapler for closing your oppenent arguments. Adds 5 damage.",
+        image: asset_server.load("items/item_stapler.png"),
+        drop_rate: 0.9,
+        item: Item::Stapler,
+    });
+    items.0.push(ItemInfo {
+        description: "With stickynotes you never forget about deadlines. +2% defence.",
+        image: asset_server.load("items/item_stickynotes.png"),
+        drop_rate: 0.9,
+        item: Item::Stickynotes,
     });
 
     commands.insert_resource(items);
